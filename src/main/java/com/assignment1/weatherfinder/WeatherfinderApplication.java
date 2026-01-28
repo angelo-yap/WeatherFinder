dc78f6e94608a2a29198cd33268274f3d7c67fd4
qt3r <angeloyap0329@gmail.com>
2026-01-28 13:32:08 -0800

    Initial Commit

---
+++ src/main/java/com/assignment1/weatherfinder/WeatherfinderApplication.java
@@ -0,0 +1,13 @@
+package com.assignment1.weatherfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherfinderApplication.class, args);
+	}
+
+}
